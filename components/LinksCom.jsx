838110044f1b45bfc5c668424988a81cdd2a074d
Ryan McCarthy <rmmcc92@aol.com>
2022-05-27 22:09:20 -0400

    added links to links component

--- components/LinksCom.jsx
+++ components/LinksCom.jsx
@@ -2,6 +2,10 @@ import { Canvas } from "@react-three/fiber";
 import Material from "./Material";
 import style from "../styles/Popout.module.css";
 import Popout from "./Popout";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import { Box, Typography } from "@mui/material";
 
 const LinksCom = (props) => {
   return (
@@ -9,20 +13,45 @@ const LinksCom = (props) => {
       <Canvas className={style.popOutCanvas}>
         <Material />
       </Canvas>
-      <div className={style.gridText}>
-        <h1>Links</h1>
-        <ol>
+      <Box
+        className={style.gridText}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: "12em",
+        }}
+      >
+        <Typography variant="h1" component="h1">
+          Links
+        </Typography>
+        <ul>
           <li>
-            <a href="#"> Lorem.</a>
+            <a
+              href="https://github.com/rmmccar92"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <GitHubIcon style={{ fontSize: "5em" }} />
+            </a>
           </li>
           <li>
-            <a href="#"> Lorem.</a>
+            <a
+              href="https://www.linkedin.com/in/ryan-mccarthy-20950291/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <LinkedInIcon style={{ fontSize: "5em" }} />
+            </a>
           </li>
           <li>
-            <a href="#"> Lorem.</a>
+            <a href="mailto:rmmccar92@gmail.com">
+              <MailOutlineIcon style={{ fontSize: "5em" }} />
+            </a>
           </li>
-        </ol>
-      </div>
+        </ul>
+      </Box>
     </Popout>
   );
 };
